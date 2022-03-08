6decd67b53bf3dae66848662e82fccb431d58d56
André Schreck <moin@andreschreck.de>
2022-03-08 19:38:40 +0100

    topic form

--- frontend/src/CourseElement.tsx
+++ frontend/src/CourseElement.tsx
@@ -6,7 +6,8 @@ export default function CourseElement() {
 
     const params = useParams();
     const [course, setCourse] = useState({} as Course);
-    const [newTopic, setNewTopic] = useState('');
+    const [newTopicName, setNewTopicName] = useState('');
+    const [newTopicDescription, setNewTopicDescription] = useState('');
 
     const fetchCourse = useCallback(() => {
         fetch(`${process.env.REACT_APP_BASE_URL}/api/courses/${params.courseId}`)
@@ -17,8 +18,17 @@ export default function CourseElement() {
     useEffect(() => fetchCourse(), [fetchCourse]);
 
     const addTopic = () => {
-        fetch(`${process.env.REACT_APP_BASE_URL}/api/courses/${params.courseId}/topics`)
-            .then(() => fetchCourse());
+        fetch(`${process.env.REACT_APP_BASE_URL}/api/courses/${params.courseId}/topics`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: newTopicName,
+                description: newTopicDescription
+            })
+        })
+        .then(() => fetchCourse());
     };
 
     return (
@@ -29,7 +39,8 @@ export default function CourseElement() {
                     <div>
                         <h2>Topics</h2>
                         <div>
-                            <input type="text" placeholder="Topic" value={newTopic} onChange={ev => setNewTopic(ev.target.value)} />
+                            <input type="text" placeholder="Topic" value={newTopicName} onChange={ev => setNewTopicName(ev.target.value)} />
+                            <textarea placeholder="Beschreibung" value={newTopicDescription} onChange={ev => setNewTopicDescription(ev.target.value)}></textarea>
                             <button onClick={addTopic}>Speichern</button>
                         </div>
                         <ul>
