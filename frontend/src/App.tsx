fdd6b22e64084826f58f302917c7779d3c45e5fe
André Schreck <moin@andreschreck.de>
2022-03-17 16:26:46 +0100

    oauth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ function App() {
         <div>
             <Link to="/registration">Register</Link>
             <Link to="/login">Login</Link>
+            <a href="https://github.com/login/oauth/authorize?client_id=ac870a1600ec03b7be10">GitHub</a>
         </div>
     );
 }
