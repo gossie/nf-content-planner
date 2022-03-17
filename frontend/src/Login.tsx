202b71d4f2e9be95c715e2a5074d50ac388af306
André Schreck <moin@andreschreck.de>
2022-03-17 17:29:47 +0100

    oauth

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -44,7 +44,7 @@ export default function Login() {
                 <Button label={t('buttonLogin')} onClick={login} />
             </div>
             <div>
-                <a href="/oauth2/authorization/github">GitHub</a>
+                <a href="https://github.com/login/oauth/authorize?client_id=ac870a1600ec03b7be10">GitHub</a>
             </div>
         </div>
     )
