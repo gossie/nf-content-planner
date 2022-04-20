272ad28362b784f8485cc3de5a8647e15903e938
André Schreck <gossie@users.noreply.github.com>
2022-04-20 10:01:00 +0200

    Delete CsrfTokenDocument.java

--- backend/app/src/main/java/com/github/gossie/nf/planner/security/CsrfTokenDocument.java
+++
@@ -1,8 +0,0 @@
-package com.github.gossie.nf.planner.security;
-
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document("csrfTokens")
-public record CsrfTokenDocument(@Id String id, String csrfToken) {
-}
